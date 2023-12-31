c7fcc630358dc953f1368a7b724a092200c19405
ohokusan <ohoku@yahoo.com>
2023-12-31 01:46:12 +0000

    Add little changes for accessibility

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let charactersAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-let charactersNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let charactersSpecial = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+const charactersAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const charactersNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const charactersSpecial = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 console.log(charactersAlphabet.length)
 console.log(charactersNumbers.length)
 console.log(charactersSpecial.length)
